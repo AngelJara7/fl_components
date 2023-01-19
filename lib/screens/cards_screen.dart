4a39202c51f38a2c5ddc86832c546b9806083888
Angel <angeljara07@outlook.com>
2023-01-19 15:36:25 -0500

    imagenes como argumentos en custom_card_type_2.dart

--- lib/screens/cards_screen.dart
+++ lib/screens/cards_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import '../widgets/widgets.dart';
@@ -17,7 +19,12 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10,),
-          CustomCardType2()
+          CustomCardType2(name: "Un hermoso paisaje", imageUrl: "https://photographylife.com/wp-content/uploads/2017/01/What-is-landscape-photography.jpg",),
+          SizedBox(height: 10,),
+          CustomCardType2(imageUrl: "https://iso.500px.com/wp-content/uploads/2014/06/W4A2827-1.jpg",),
+          SizedBox(height: 10,),
+          CustomCardType2(imageUrl: "https://images.squarespace-cdn.com/content/v1/59523d5c4c8b031b6d9dcb5b/1645865436351-NF1WX4AHJUE43OZ3GJCY/_S6A1420-Edit-Edit.jpg?format=2500w",),
+          SizedBox(height: 100,)
         ],
       ),
     );
